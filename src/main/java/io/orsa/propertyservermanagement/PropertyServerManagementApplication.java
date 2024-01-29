c3512f1e4fa47d63a62763630e0f24069ac1d17a
Trong Dat <datnt700@gmail.com>
2024-01-29 12:27:07 +0100

    first commit

---
+++ src/main/java/io/orsa/propertyservermanagement/PropertyServerManagementApplication.java
@@ -0,0 +1,13 @@
+package io.orsa.propertyservermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyServerManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyServerManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/orsa/propertyservermanagement/PropertyServerManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package io.orsa.propertyservermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyServerManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
